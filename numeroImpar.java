1a84b9f46213cd67f4cc637ef052c38d76f13598
Gabriellima21 <62075965+Gabriellima21@users.noreply.github.com>
2022-09-19 21:06:56 -0300

    Números Impares - While

--- numeroImpar.java
+++ numeroImpar.java
@@ -2,12 +2,13 @@
 public class numeroImpar {
 
 	public static void main(String[] args) {
-		for (int i = 1; i<=100; i++) {
-			if (i % 2 !=0) {
+		int i = 0;
+		while (i<=100) {
+			if(i % 2 !=0) {
 				System.out.println(i);
+				i++;
 			}
 		}
-		
 
 	}
 
