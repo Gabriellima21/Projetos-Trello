ffdc1f8df1f86a69c29ee0b8c893f2637735ff21
Gabriellima21 <62075965+Gabriellima21@users.noreply.github.com>
2022-09-19 21:00:52 -0300

    contador 100 a 1 - While

--- contadorWhile.java
+++ contadorWhile.java
@@ -2,13 +2,12 @@
 public class contadorWhile {
 
 	public static void main(String[] args) {
-		int i = 1;
 		
-		while (i<=100) {
+		int i= 100;
+		while (i<=1) {
 			System.out.println(i);
-			i++;
+			i--;
 		}
-
 	}
 
 }
