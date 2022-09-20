26bc448efc37de8fb06be4b0d410bd1be54352ff
Gabriellima21 <62075965+Gabriellima21@users.noreply.github.com>
2022-09-19 21:08:24 -0300

    Números Pares - For

--- NumeroPar.java
+++ NumeroPar.java
@@ -2,13 +2,12 @@
 public class NumeroPar {
 
 	public static void main(String[] args) {
-		int i = 1; 
-		do {
+		for (int i = 1; i<=100; i++) {
 			if (i % 2 == 0) {
 				System.out.println(i);
-				i++;
 			}
-		}while (i<=100); 
+		}
+
 	}
 
 }
