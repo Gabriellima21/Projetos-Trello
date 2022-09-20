91ddb1257e42f3f802a1cb25837fa3640bb36db8
Gabriellima21 <62075965+Gabriellima21@users.noreply.github.com>
2022-09-19 21:09:03 -0300

    Números Pares - Foreach

---
+++ numerosPares.java
@@ -0,0 +1,18 @@
+
+public class NumerosPares {
+
+	public static void main(String[] args) {
+		int[] numeros = new int [100];
+		
+		for(int i = 0; i<numeros.length; i++) {
+			numeros[i] = i + 1;	
+	       }
+		for (int numero : numeros) {
+			if (numero % 2 ==0) {
+				System.out.println(numero);
+			}
+       }
+	}  
+}
+
+
