779248a39b870c421186ed4c9e41e745f247e905
Gabriellima21 <62075965+Gabriellima21@users.noreply.github.com>
2022-09-19 21:10:54 -0300

    remover número e exibir na ordem - List

--- Ordem.java
+++ Ordem.java
@@ -1,31 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Ordem {
 
 	public static void main(String[] args) {
-		int[] numeros = new int [5];
+		List <Integer> sequencia = new ArrayList<>();
 		
 		Scanner input = new Scanner (System.in);
 		
-		for (int i=0; i<numeros.length;i++) {
+		for(int i=0; i<5;i++) {
 			System.out.println("Informe um número: ");
-			int n = input.nextInt();
-			numeros[i] = n;
-		}
-		
-		for (int i=0; i<numeros.length -1; i++) { //for que percorre ate a penultima posição do array
-			for(int j=i +1; j<numeros.length;j++) { //for que compara a posição atual com a seguinte.
-				if (numeros[i]>numeros[j]) {
-					int troca = numeros [i]; //guardei o valor da posição a na variavel troca
-					numeros[i] = numeros [j];
-					numeros[j] = troca;
-				}
-			}//3,1,4,5,6
-			 //1,3,4,5,6
-		}
-		for (int i=0;i<numeros.length;i++) {
-			System.out.println(numeros[i]);
-		}
+			 sequencia.add(input.nextInt());
+	    }
+		System.out.println(sequencia);
+    }
+}
 
-	}
 
-}  //algoritimo de ordenação
+/*sequencia.add(4);
+sequencia.add(9);
+sequencia.add(1);
+sequencia.add(1);
+sequencia.add(8);
+
+System.out.println(sequencia);*/
+
+/*Scanner input = new Scanner (System.in);
+
+for(int i=0; i<=5;i++) {
+	System.out.println("Informe um número: ");
+	int n = input.nextInt();
+	sequencia.add(i) += n;
+}*/
